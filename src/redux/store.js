5e9d59c848396243701dd382adda16a4f577121e
Jerome Haas <j.haas@cyberplanet.ch>
2021-02-26 16:52:41 +0100

    Recactor: remove the dependency for the redux-dev-tools in the store for production

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,9 +8,16 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  )
+    applyMiddleware(thunk))
 );
+// FOR DEVELOPMENT USE THIS ONE
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//   )
+// );
 
 export default store;
